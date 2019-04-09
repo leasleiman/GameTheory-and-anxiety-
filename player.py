7bc4e84002357e2163426115ee70543c463b390a
chingchinghuang <huang27c@mtholyoke.edu>
2019-04-09 11:29:56 -0400

    Add comment

--- player.py
+++ player.py
@@ -5,17 +5,20 @@ from math import *
 
 class Player:
 
-	#create a player with given function which is a string
+	#create a player with given function which is a string 
 	def __init__(self, function):
 		self.function = function
+		self.profit = 0
 
 	#get the anxiety level with given time
 	def getAnxietyLevel(self, x):
 		func = self.function.replace('x', str(x))
 		return eval(func)
 
+	#set the profit 
 	def setProfit(self, profit):
 		self.profit = profit
 
+	#get the profit
 	def getProfit(self):
 		return self.profit
